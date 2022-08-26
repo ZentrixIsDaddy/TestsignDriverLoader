e45af8275d54e34b66713048bd31ec4cb10128d4
ZentrixIsTheDude <67102323+ZentrixIsDaddy@users.noreply.github.com>
2022-08-25 18:36:09 -0700

    Add files via upload
    name the driver u wanna load as "Drv.sys"

---
+++ loader/ConsoleApp5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n [IMPORTANT] Please make sure to turn bcdedit on before accusing it of not working");
+            Console.WriteLine("\n [LOADING] Finding Drv.sys");
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\Drv.sys"))
+            {
+                Console.WriteLine(" [LOADED] Found Drv.sys!");
+                Console.WriteLine(" [LOADING] Loading Drv.sys...");
+                Process.Start("Cmd.exe", "/c sc create GGKernelLoader binpath" + Directory.GetCurrentDirectory() + "\\Drv.sys" + " type=kernel");
+                Process.Start("Cmd.exe", "/c sc start GGKernelLoader");
+                Console.WriteLine(" [LOADED] Loaded Drv.sys!");
+            }
+            else
+            {
+                Console.WriteLine(" [ERROR] Didnt find Drv.sys!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
